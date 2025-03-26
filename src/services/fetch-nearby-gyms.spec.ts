3647da4565f16dcf6ad603b48ecd52fc0571543b
degui1 <gui.denez56@gmail.com>
2025-03-26 08:14:37 -0300

    Add: fetch nearby gyms use case

---
+++ src/services/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { FetchNearbyGymsService } from './fetch-nearby-gyms.service'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsService
+
+describe('Fetch nearby Gyms service', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsService(gymsRepository)
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      latitude: -20.3303171,
+      longitude: -47.7948197,
+      phone: null,
+    })
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      latitude: -16.8613945,
+      longitude: -44.9087921,
+      phone: null,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -20.3295902,
+      userLongitude: -47.7931372,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
